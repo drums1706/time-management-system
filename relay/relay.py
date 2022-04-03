62e06463654e00ba113b5215da286c2c17e47557
easyboy1706 <62602436+easyboy1706@users.noreply.github.com>
2022-04-03 16:33:25 +0200

    Update relay.py

--- relay/relay.py
+++ relay/relay.py
@@ -4,7 +4,8 @@ import requests
 
 app = Flask(__name__)
 
-url = "https://prod-99.westeurope.logic.azure.com:443/workflows/12f484e2182142a3809dbf8283d2aa28/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5eoc065JdR6Cr0J32dKOWdZCEtBIpCqKKAxNl77wQDs"
+#Add Url here
+url = ""
 
 @app.route('/', methods=['POST'])
 def index():
